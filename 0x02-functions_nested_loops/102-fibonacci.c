0f3bb9b4c409329990e5ea7a96d3719ec334ef13
Enock Yeboah-Mensah <eymensah@gmail.com>
2022-12-13 21:27:14 -0800

    102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,18 +10,18 @@ int main(void)
 	long j = 1, k = 2;
 	{
 		if (i == 0)
-		printf("%d\n", j);
+		printf("%ld\n", j);
 		else
 		if (i == 1)
-		printf("%d\n", k);
+		printf("%ld\n", k);
 		else
 		{
 		k += j;
 		j = k - j;
-		printf("%d\n", k);
+		printf("%ld\n", k);
 		}
-		++1
+		++1;
 	}
-	printf("%d\n");
+	printf("%ld\n");
 	return (0);
 }
