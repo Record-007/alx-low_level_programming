e8a9a39cfdc1eaaa4dbf94cc2b721d8f87eb82a3
Enock Yeboah-Mensah <eymensah@gmail.com>
2022-12-14 08:38:58 -0800

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,18 @@
 #include <stdio>
 
 /**
- * main - Prints the alphabet in lowercase, except for q and e.
- * Return: Always 0.
+ * main -> assign a random number to the variable n each time it is executed
+ * and print the last digit of the number stored in the variable n
+ * Return: Always 0 (Success)
 */
 int main(void)
 {
-	char letter;
+	int n;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (n = 0; n < 10; n++)
 	{
-		if (letter != 'e' && letter != 'q')
-		putchar(letter);
+		printf("%d", n);
 	}
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
